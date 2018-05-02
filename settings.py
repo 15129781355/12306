66db2c21dad4883a0916b908288bea7c26da6876
15129781355 <38899763+15129781355@users.noreply.github.com>
2018-05-02 13:59:53 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,49 @@
+
+'''1预定  3车次 8出发时间 9到达时间 10历时 21 高级软卧 23软卧  25,32商务座特等座
+ 31一等座  30二等座 26无座 29 硬座 28硬卧 33 动卧  27 软座'''
+TRAIN_DATE='2018-05-16'
+FROM_STATION = '成都'
+TO_STATION = '长沙'
+NOW_TIME = '2018-05-02'
+SET = 23
+
+header = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+    'Referer':'https://kyfw.12306.cn/otn/passport?redirect=/otn/login/userLogin'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
